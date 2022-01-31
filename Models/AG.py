84977af4e752e71b44b95c1e81bbec6d53483252
JuanChandoqui <193242@ids.upchiapas.edu.mx>
2022-01-30 23:22:04 -0600

    interface added

---
+++ Models/AG.py
@@ -0,0 +1,5 @@
+##METODO PARA AGREGAR LA LOGICA
+
+# def solveSudoku(list_Grid):
+#     listadeResultados = []
+#     return listadeResultados
---
+++ main.py
@@ -0,0 +1,61 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic, QtGui, QtCore
+from threading import Thread
+from time import sleep
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('./Views/menu.ui', self)   
+
+        self.pushButton_ResolverSudoku.clicked.connect(self.pushButtonSolveSudoku)
+
+
+    def pushButtonSolveSudoku(self):
+        list_Grid = []
+        list_Grid_with_0 = []
+
+        for i in range(9):
+            list_aux = []
+            for j in range(9):
+                number = getattr(self, "line_" + str(i) + "_" + str(j)).text()
+                if (number == "" or number == " " or number == "  "):
+                    list_aux.append(0)               
+                else:
+                    number = int(number)
+                    list_aux.append(number)
+            
+            list_Grid_with_0.append(list_aux) #para mantener los valores en color negro
+            list_Grid.append(list_aux)
+
+        self.threadSolveSudoku(list_Grid=list_Grid, list_Grid_with_0=list_Grid_with_0)
+
+
+    def fillSudoku(self, list_Grid:list, list_Grid_with_0: list):
+        Palette= QtGui.QPalette()
+
+        for i in range(9):
+            for j in range(9):
+                if(list_Grid_with_0[i][j] == 0):
+                    getattr(self, "line_"+str(i)+"_"+str(j)).setText(f'{1}')
+                    # getattr(self, "line_"+str(i)+"_"+str(j)).setText(f'{list_Grid[i][j]}') #TODO: <---- colocar el valor deseado a solucionar en setText()
+                    getattr(self, "line_"+str(i)+"_"+str(j)).setFont(QtGui.QFont("Times",weight=QtGui.QFont.Bold))
+                    Palette.setColor(QtGui.QPalette.Text, QtCore.Qt.white)
+                    getattr(self, "line_"+str(i)+"_"+str(j)).setPalette(Palette)
+                    sleep(0.01)
+
+
+    def threadSolveSudoku(self, list_Grid, list_Grid_with_0):
+        thread = Thread(target=self.fillSudoku, args=(list_Grid, list_Grid_with_0,))
+        thread.start() 
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    demo = Window()
+    demo.show()
+
+    try: 
+        sys.exit(app.exec_())
+    except SystemExit:
+        print('Closing Window...')
